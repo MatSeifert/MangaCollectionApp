efc0ddc0eab041ef2328960daa725794def3ac8c
MatSeifert <matthe.seifert@gmail.com>
2016-04-14 13:53:22 +0200

    minor changes

--- app/src/main/java/matthiasseifert/de/sakuranimangacollection/AddNewItemActivity.java
+++ app/src/main/java/matthiasseifert/de/sakuranimangacollection/AddNewItemActivity.java
@@ -6,12 +6,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import matthiasseifert.de.sakuranimangacollection.model.Book;
+
 public class AddNewItemActivity extends AppCompatActivity {
+    Book _book;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addnewitem);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        assert toolbar != null;
+        toolbar.setSubtitle(R.string.addNewItem_Title);
+
+        _book = new Book();
     }
 }
--- app/src/main/java/matthiasseifert/de/sakuranimangacollection/MainActivity.java
+++ app/src/main/java/matthiasseifert/de/sakuranimangacollection/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        assert toolbar != null;
+        toolbar.setSubtitle(R.string.mainActivity_Title);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
