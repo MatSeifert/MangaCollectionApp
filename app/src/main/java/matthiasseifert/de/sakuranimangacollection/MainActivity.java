5bb0a00e453764d194ba7adb4b129761c21fc759
MatSeifert <matthe.seifert@gmail.com>
2016-04-19 14:03:38 +0200

    AddNewItem Activity überarbeitet

--- app/src/main/java/matthiasseifert/de/sakuranimangacollection/MainActivity.java
+++ app/src/main/java/matthiasseifert/de/sakuranimangacollection/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        assert toolbar != null;
-        toolbar.setSubtitle(R.string.mainActivity_Title);
+
+        // Set the Title
+        TextView headline = (TextView) findViewById(R.id.headline);
+        assert headline != null;
+        headline.setText(R.string.mainActivity_Title);
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
